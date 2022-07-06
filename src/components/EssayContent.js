af228aa1a7d8a6fbd79c57df288adffe535adf2f
ciaociaoworld <yellowzung@gmail.com>
2022-07-06 15:02:25 +1000

    change for edit

--- src/components/EssayContent.js
+++ src/components/EssayContent.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import React from 'react'
+import Axios from 'axios'
 
 import AddYourEssay from './AddYourEssay'
 
@@ -9,24 +10,28 @@ function EssayContent() {
    
 
     const addEssay = () => {
-        if(newEssay) {
+        if (newEssay) {
             let num = essays.length
             let newEssayObj = { id: num, content: newEssay }
             setEssays([...essays, newEssayObj])
             setNewEssay('')
+            Axios.post('http://localhost:3001', newEssayObj).then((res) => {
+                res.data
+            })
         }
     }
 
     const deleteEssay = (id) => {
-        let idTouch = essays.filter( essay => essay.id !== id)
+        let idTouch = essays.filter(essay => essay.id !== id)
         setEssays(idTouch)
+        Axios.delete('http://localhost:3001', { id })
     }
 
-    
 
-    const editEssay = (content) => {
-        
 
+    const editEssay = (index) => {
+        essays[index].isEdit = false
+        Axios.put('http://localhost:3001', essays[index]) 
     }
 
 
@@ -55,29 +60,36 @@ function EssayContent() {
 
     return (
         <div className='container'>
-            <div className='showAll'> 
+            <div className='showAll'>
                 <h3>Your Secret Diary</h3>
-                
-                {essays.map((essay) => {
-                   return <div className='essayContainer'>
-                        <p className='essayContent' id={essay.id}>{essay.content} </p>
-                    
+
+                {essays.map((essay, index) => {
+                    return <div className='essayContainer'>
+                        {
+                            essay.isEdit
+                                ? <input value={essay.content} onInput={({ target }) => { essay.content=target.value }} />
+                                : <p className='essayContent' id={essay.id}>{essay.content} </p>
+                        }
                         <button onClick={() => deleteEssay(essay.id)}>Delete</button>
-                    
-                        <button onClick={() => editEssay(essay.content) }>Update</button>
+
+                        {!essay.isEdit
+                            ? <button onClick={essay.isEdit=true}>Edit</button>
+                            : <button onClick={() => editEssay(index)}>Save</button>
+                        }
                     </div>
                 })}
-                
+
+
             </div>
 
             <div className='EssayInput'>
                 <h3>Add Your Secret</h3>
-                    <AddYourEssay
-                        addEssay={addEssay}
-                        newEssay={newEssay}
-                        setNewEssay={setNewEssay}
-            
-                    /> 
+                <AddYourEssay
+                    addEssay={addEssay}
+                    newEssay={newEssay}
+                    setNewEssay={setNewEssay}
+
+                />
             </div>
         </div>
     )
